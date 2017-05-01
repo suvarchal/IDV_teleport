609b829d6d2aca9f4c84819c02e1fbc4af212d71
Suvarchal Kumar Cheedela <suvarchal@users.noreply.github.com>
2017-05-01 14:44:56 -0400

    Add files via upload

---
+++ run_script_example.sh
@@ -0,0 +1,13 @@
+python bundlescript.py -t 2005-07-06_22:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 17 76 23 84 -case lat_20_lon_80
+python bundlescript.py -t 2005-07-01_16:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 13 72 19 80 -case lat_16_lon_76
+python bundlescript.py -t 2005-06-18_08:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 13 80 19 88 -case lat_16_lon_84
+python bundlescript.py -t 2005-06-28_22:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 5 60 11 68 -case lat_08_lon_64
+python bundlescript.py -t 2005-10-01_09:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 5 60 11 68 -case lat_08_lon_64
+python bundlescript.py -t 2005-09-28_14:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 5 60 11 68 -case lat_08_lon_64
+python bundlescript.py -t 2005-08-02_16:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 17 72 23 80 -case lat_20_lon_76
+python bundlescript.py -t 2005-06-18_07:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 9 68 15 76 -case lat_12_lon_72
+python bundlescript.py -t 2005-07-16_13:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 13 68 19 76 -case lat_16_lon_72
+python bundlescript.py -t 2005-10-01_10:30:00  -b G5NR_momflux_bundle.xidv -td 4hours -bbox 5 56 11 64 -case lat_08_lon_60
+python bundlescript.py -t 2005-08-17_13:30:00  -b G5NR_momflux_bundle.xidv -td 4hours -bbox 9 72 15 80 -case lat_12_lon_76
+
+
