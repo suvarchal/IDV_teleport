eda80b444a92ac55bc43c43f6b00c1a32074d7fa
suvarchal <suvarchal.kumar@gmail.com>
2018-05-12 22:22:24 -0400

    changes to runscript to have new executable

--- run_script_example.sh
+++ run_script_example.sh
@@ -1,1 +1,1 @@
-python idv_teleport.py -t 2005-07-06_22:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 17 76 23 84 -case lat_20_lon_80
+idv_teleport -t 2005-07-06_22:30:00  -b G5NR_momflux_bundle.xidv -td 1hours -bbox 17 76 23 84 -case lat_20_lon_80
