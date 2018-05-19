06287c6afecbd7620b94cefd587109917e498867
suvarchal <suvarchal.kumar@gmail.com>
2018-05-19 08:41:32 +0200

    fixes to doc

--- docs/searchindex.js
+++ docs/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["examples","index","install"],envversion:53,filenames:["examples.rst","index.rst","install.rst"],objects:{},objnames:{},objtypes:{},terms:{},titles:["Examples","&lt;no title&gt;","Install"],titleterms:{exampl:0,instal:2}})
+Search.setIndex({docnames:["examples","index","install"],envversion:53,filenames:["examples.rst","index.rst","install.rst"],objects:{},objnames:{},objtypes:{},terms:{},titles:["Examples","IDV_teleport","Install"],titleterms:{exampl:0,idv_teleport:1,instal:2}})
