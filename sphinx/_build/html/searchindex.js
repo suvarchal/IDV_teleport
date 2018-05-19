8988824de7541af5cd4890b64029d237533417ed
suvarchal <suvarchal.kumar@gmail.com>
2018-05-19 07:55:24 +0200

    update docs

---
+++ sphinx/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["examples","index","install"],envversion:53,filenames:["examples.rst","index.rst","install.rst"],objects:{},objnames:{},objtypes:{},terms:{},titles:["Examples","&lt;no title&gt;","Install"],titleterms:{exampl:0,instal:2}})
--- sphinx/index.rst
+++ sphinx/index.rst
@@ -10,3 +10,6 @@
 
    install
    examples
+====================
+IDV_teleport
+===================
