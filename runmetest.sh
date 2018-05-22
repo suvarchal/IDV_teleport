e35a4192fb1fed580e43143d90dc50c98aef9989
suvarchal <suvarchal.kumar@gmail.com>
2018-05-22 23:42:00 +0200

    testing IDV run 6

--- runmetest.sh
+++ runmetest.sh
@@ -3,4 +3,7 @@ curl -s ftp://ftp.unidata.ucar.edu/pub/idv/nightly_idv_5.5/idv_5_5_linux64_insta
 chmod +x idv.sh
 echo `pwd`
 printf 'o\n\n1\n'`pwd`'/IDV\n'|./idv.sh
-./IDV/runIDV
+wkdir=`echo pwd`
+export IDV_HOME=${wrkdir}/IDV
+cd test
+idv_teleport -b NOAA_sst.xidv -t 2011-01-01
