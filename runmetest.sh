e5a04164d85e55f4ad0d08cc00396afac2a3856c
suvarchal <suvarchal.kumar@gmail.com>
2018-05-28 15:23:52 +0200

    fix font rendering in xvfb

--- runmetest.sh
+++ runmetest.sh
@@ -8,7 +8,7 @@ export IDV_HOME=${wrkdir}/IDV
 mkdir -p ${wrkdir}/.java/.systemPrefs
 mkdir ${wrkdir}/.java/.userPrefs
 chmod -R 755 ${wrkdir}/.java
-export JAVA_OPTS="-Djava.util.prefs.systemRoot="${wrkdir}"/.java -Djava.util.prefs.userRoot="${wrkdir}"/.java/.userPrefs"
+export JAVA_OPTS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true -Djava.util.prefs.systemRoot="${wrkdir}"/.java -Djava.util.prefs.userRoot="${wrkdir}"/.java/.userPrefs"
 #export JAVA_OPTS="-Djava.util.prefs.systemRoot="`pwd`"/.java -Djava.util.prefs.userRoot="`pwd`"/.java/.userPrefs"
 cd test
 echo $HOME
